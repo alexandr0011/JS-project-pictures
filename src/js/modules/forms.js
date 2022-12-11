f885c526d488dd35804125b869c974f297927373
Alexandr <alexandrpo271@gmail.com>
2022-12-11 22:20:12 +0300

    added get data and create cards elements functionality

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,3 +1,5 @@
+import { postData } from "../services/requests";
+
 const forms = () => {
     const form = document.querySelectorAll('form');
     const inputs = document.querySelectorAll('input');
@@ -17,15 +19,6 @@ const forms = () => {
         designer: 'assets/server.php',
         question: 'assets/question.php'
     }
-
-    const postData = async (url, data) => {
-        let res = await fetch(url, {
-            method: 'POST',
-            body: data
-        });
-
-        return await res.text();
-    };
 
     const clearInputs = () => {
         inputs.forEach(item => {
